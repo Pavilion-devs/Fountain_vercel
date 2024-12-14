5807e6db8a38b823b7f36da81056b7a7849654c9
Pavilion <118903029+Pavilion-devs@users.noreply.github.com>
2024-12-14 20:49:11 +0100

    Update Hero.tsx

--- src/app/landing-page/Hero.tsx
+++ src/app/landing-page/Hero.tsx
@@ -1,13 +1,20 @@
 'use client';
 
 import React from "react";
-import "../../styles/Hero.css"
+import "../../styles/Hero.css";
+import img from "../../images/herobackgroundImg.png";
 import Image from 'next/image';
 import { mainheroImg } from "@/images";
 
 const Hero = () => {
   return (
-    <section className="hero">
+    <section className="hero" style={{
+      backgroundImage: `url(${img.src})`, 
+      backgroundSize: 'cover', 
+      backgroundPosition: 'center', 
+      backgroundRepeat: 'no-repeat',
+      zIndex: -1,
+    }}>
       <div className="hero__content">
         <div className="hero_content_text">
           <h1 className="hero__title">Fuel your day with <span className="cursive">quality</span></h1>
